871c7a3e400e8b91e5ab3afab781ff914c8cd2f1
Christian Wansart <christian@bluepanel.org>
2015-05-24 19:40:44 +0200

    print key code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main(int argc, char const *argv[]) {
   sf::RenderWindow window(sf::VideoMode(800,600), "SFML App");
+
   sf::CircleShape shape;
   shape.setRadius(20.0f);
   shape.setPosition(100.0f, 100.0f);
   shape.setFillColor(sf::Color::Blue);
+
   while(window.isOpen()) {
     sf::Event event;
     while(window.pollEvent(event)) {
       if(event.type == sf::Event::Closed) {
         window.close();
+      }
+
+      if(event.type == sf::Event::KeyPressed) {
+        std::cout << "key pressed: " << event.key.code << std::endl;
       }
     }
 
